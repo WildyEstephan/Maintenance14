8bf39d96eac2b9b3ec9c1bcdbf3482b0c93cb5bc
Wildy Estephan <westephan50@gmail.com>
2022-08-08 13:14:40 -0400

    Update sale.py

--- maintenance_field_service/models/sale.py
+++ maintenance_field_service/models/sale.py
@@ -51,3 +51,29 @@ class SaleOrder(models.Model):
                     'partner_id': self.partner_id.id,
                     'sale_id': self.id
                 })
+
+
+    def action_view_delivery(self):
+
+        action = self.env["ir.actions.actions"]._for_xml_id("maintenance_field_service.installation_request_action")
+
+        installations = self.mapped('installation_ids')
+        if len(installations) > 1:
+            action['domain'] = [('id', 'in', installations.ids)]
+        elif installations:
+            form_view = [(self.env.ref('maintenance_field_service.installation_request_view_form').id, 'form')]
+            if 'views' in action:
+                action['views'] = form_view + [(state, view) for state, view in action['views'] if view != 'form']
+            else:
+                action['views'] = form_view
+            action['res_id'] = installations.id
+        # # Prepare the context.
+        # installation_id = installations.filtered(lambda l: l.picking_type_id.code == 'outgoing')
+        # if installation_id:
+        #     installation_id = installation_id[0]
+        # else:
+        #     installation_id = installations[0]
+        # action['context'] = dict(self._context, default_partner_id=self.partner_id.id,
+        #                          default_picking_type_id=installation_id.picking_type_id.id, default_origin=self.name,
+        #                          default_group_id=installation_id.group_id.id)
+        return action
