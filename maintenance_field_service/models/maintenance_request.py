e066f49c0e2230935c0dbd5044130b673cc22ce8
Wildy Estephan <westephan50@gmail.com>
2022-08-09 07:50:27 -0400

    Update maintenance_request.py

--- maintenance_field_service/models/maintenance_request.py
+++ maintenance_field_service/models/maintenance_request.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+import datetime
 
 class MaintenanceRequest(models.Model):
     _name = 'maintenance.request'
@@ -7,5 +8,45 @@ class MaintenanceRequest(models.Model):
     name = fields.Char(
         string='Name', 
         required=False)
-    
+    equipment_id = fields.Many2one(
+        comodel_name='maintenance.equipments',
+        string='Equipment',
+        required=False)
+    request_date = fields.Date(
+        string='Request Date',
+        required=False, default=datetime.datetime.today())
 
+    @api.model
+    def create(self, values):
+        # Add code here
+
+        values['name'] = self.env['ir.sequence'].next_by_code('maintenance.request')
+
+        return super(MaintenanceRequest, self).create(values)
+
+class PartsEquipments(models.Model):
+    _name = 'maintenance.parts'
+    _description = 'Parts Lines'
+
+    name = fields.Char(
+        string='Name',
+        required=False, related='product_id.name')
+    part_id = fields.Many2one(
+        comodel_name='product.template',
+        string='Product',
+        required=True, domain="[('is_part', '=', True)]")
+    serial_part = fields.Char(
+        string='Serial',
+        required=False)
+    equipment_id = fields.Many2one(
+        comodel_name='maintenance.equipments',
+        string='Equipment',
+        required=False)
+    equipment_part = fields.Many2one(
+        comodel_name='product.parts.equipments',
+        string='Equipment Part',
+        required=False)
+    maintenance_request_id = fields.Many2one(
+        comodel_name='maintenance.request',
+        string='Maintenance Request',
+        required=False)
